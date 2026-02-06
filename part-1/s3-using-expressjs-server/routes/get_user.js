ce24915ed485e1e677c3942c146226599952dd69
Ashwath Prabhu <ashwath.prabhu@7edge.com>
2026-02-06 23:17:11 +0530

    fix: Change `/` route from POST to GET method.

--- part-1/s3-using-expressjs-server/routes/get_user.js
+++ part-1/s3-using-expressjs-server/routes/get_user.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const router = express.Router()
 
-router.post('/', async (req, res) => {
+router.get('/', async (req, res) => {
     try {
         return res.status(200).json({
             data: {
